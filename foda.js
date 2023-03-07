02fc2623c058dcfc9a3f2ee5dc46061246807b9f
LeandroAndresArce <101754543+LeandroAndresArce@users.noreply.github.com>
2023-03-07 15:48:14 -0300

    Segunda entrega

--- foda.js
+++ foda.js
@@ -1,19 +1,19 @@
+const fs = require("fs")
+
 class ProductManager {
 
     static nroid = 1;
 
-    constructor (id, codigo, titulo, descripcion, precio, imagen, stock) {
+    constructor (filename) {
         this.productos = [];
-        this.codigo =codigo;
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.imagen = imagen,
-        this.stock = stock;
-        this.id=id;
+        this.path = './files'
+        this.filename = this.path + filename
+
     }
 
-addProduct(producto){
+addProduct = async (id, codigo, titulo, descripcion, precio, imagen, stock) => {
+    await fs.promises.mkdir(this.path, {recursive:true})
+
     for (const elemento of this.productos) {
         if (producto.stock < 0 || producto.precio < 0 || producto.titulo ==='' ||producto.descripcion ==='' || producto.imagen ==='') {
             return console.error('Rellene los campos, por favor.');
@@ -25,24 +25,54 @@ addProduct(producto){
     }
     producto.id = ProductManager.nroid;
     ProductManager.nroid ++;
-    this.productos.push(producto);
+    this.productos.push({id, codigo, titulo, descripcion, precio, imagen, stock});
+    await fs.promises.writeFile(this.filename, JSON.stringify(this.productos))
+    console.log('Producto agregado correctamente')
     return this.productos;
     }
 
-getProducts(producto){
+getProducts = async () => {
+    let result = await fs.promises.readFile(this.filename)
+    let parsedRes = await JSON.parse(result)
+    console.log(parsedRes)
     return this.productos;
 }
 
 
-getProductsById (id) {
-    for (const elemento of this.productos) {
-        if(elemento.id === id) {
-            return elemento.titulo;
+getProductsById = async (id) => {
+    let result = await fs.promises.readFile(this.filename)
+    let parsedRes = await JSON.parse(result)
+
+    const filteredArr = parsedRes.find(
+        (productos) => productos.prodId == id
+    ) 
+        return filteredArr ? filteredArr : ''
+}
+
+updateProductById = async(id, updatedData) =>{
+    let result = await fs.promises.readFile(this.name)
+    let parsedRes = await JSON.parse(result)
+
+    if (await this.getProductsById(id)) {
+        const newArr = parsedRes.map((item) => {
+            return id == item.prodId ? {...item, ...updatedData} : item
+        })
+        await fs.promises.writeFile(this.filename, JSON.stringify(newArr))        
+        } else {
+            console.log(`producto ID ${id} no existe`)
         }
     }
-    return console.error('Ese artículo no se encuentra');
+
+deleteProductById = async (id) =>{
+    let result= await fs.promises.readFile(this.filename)
+    let parsedRes = await JSON.parse(result)
+    
+    if (await this.getProductsById(id)){
+        const newArr = parsedRes.filter((item) => item.prodId !== id)
+        await fs.promises.writeFile(this.filename, JSON.stringify(newArr))
+        console.log('producto eliminado correctamente')
+    } else {
+        console.log (`producto ID ${id} no existe`)
+        }
+    }
 }
-
-}
-
-
